c8c27512cc903b283ae897a75ad9df469637ea01
Vinicius Vitorino <vitorino.developer@outlook.com>
2021-07-29 16:24:11 -0300

    Construção da view home do painel

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -7,5 +7,44 @@
 @endsection
 
 @section('content')
+<div class="row">
+    <div class="col-lg-4 col-6">
+        <div class="small-box bg-success">
+            <div class="inner">
+                <h3>@if(array($clientes) > 0){{count($clientes)}}@endif</h3>
+                <p>@if(count($clientes) <= 1) Cliente @else Clientes @endif</p>
+            </div>
+            <div class="icon">
+                <i class="fas fa-user">
+                </i>
+            </div>
+        </div>
+    </div>
 
+    <div class="col-lg-4 col-6">
+        <div class="small-box bg-warning">
+            <div class="inner">
+                <h3>@if(array($compromissos) > 0){{count($compromissos)}}@endif</h3>
+                <p>@if(count($compromissos) <= 1) Compromisso @else Compromissos @endif</p>
+            </div>
+            <div class="icon">
+                <i class="fas fa-calendar-alt">
+                </i>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-lg-4 col-6">
+        <div class="small-box bg-info">
+            <div class="inner">
+                <h3>@if(array($processos) > 0){{count($processos)}}@endif</h3>
+                <p>@if(count($processos) <= 1) Processo @else Processos @endif</p>
+            </div>
+            <div class="icon">
+                <i class="fas fa-folder">
+                </i>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
