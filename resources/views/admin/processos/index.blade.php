0da03d7be95a9fb68ab1ff0aef4de3c5e072a1cb
Vinicius Vitorino <vitorino.developer@outlook.com>
2021-07-30 11:12:54 -0300

    Ajuste no tamanho do card

--- resources/views/admin/processos/index.blade.php
+++ resources/views/admin/processos/index.blade.php
@@ -12,7 +12,7 @@
     </h1>
 @endsection
 @section('content')
-<div class="card container">
+<div class="card col-12">
     <div class="card-body">
         <table class="table table-hover">
             <thead>
