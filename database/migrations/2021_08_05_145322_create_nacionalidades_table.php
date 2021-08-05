aed7a40921d0445b2d7c715bef2d91897417d550
Vinicius Vitorino <vitorino.developer@outlook.com>
2021-08-05 16:41:09 -0300

    Criação da entidade nacionalidades.

---
+++ database/migrations/2021_08_05_145322_create_nacionalidades_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNacionalidadesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('nacionalidades', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('pessoa_fisica_id')->unsigned();
+            $table->foreign('pessoa_fisica_id')
+            ->references('id')
+            ->on('pessoas_fisicas')
+            ->onDelete('cascade');
+            $table->string('tipo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('nacionalidades');
+    }
+}
