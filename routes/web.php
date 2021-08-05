7e8c1105bd36640621ed5891f50688c0939edb4d
Vinicius Vitorino <vitorino.developer@outlook.com>
2021-08-05 16:32:56 -0300

    Organização das rotas.

--- routes/web.php
+++ routes/web.php
@@ -21,23 +21,23 @@ Route::get('/', [Sistema\HomeController::class, 'index']);
 
 Route::prefix('painel')->group(function ()
 {
-    Route::get('/', [Admin\AdminController::class, 'index'])->name('admin');
-    Route::get('login', [Auth\LoginController::class, 'index'])->name('login');
+    Route::get('/',      [Admin\AdminController::class, 'index'])->name('admin');
+    Route::get('login',  [Auth\LoginController::class, 'index'])->name('login');
     Route::post('login', [Auth\LoginController::class, 'autenticacao']);
 
-    Route::get('registro', [Auth\RegistroController::class, 'index'])->name('registro');
+    Route::get('registro',  [Auth\RegistroController::class, 'index'])->name('registro');
     Route::post('registro', [Auth\RegistroController::class, 'registro']);
 
     Route::post('logout', [Auth\LoginController::class, 'logout'])->name('logout');
 
-    Route::resource('usuarios', Admin\UsuariosController::class);
-    Route::resource('paginas', Admin\PaginasController::class);
-    Route::resource('cadastro', Admin\CadastroController::class);
-    Route::resource('processo', Admin\ProcessoController::class);
+    Route::resource('usuarios',  Admin\UsuariosController::class);
+    Route::resource('paginas',   Admin\PaginasController::class);
+    Route::resource('cadastro',  Admin\CadastroController::class);
+    Route::resource('processo',  Admin\ProcessoController::class);
 
-    Route::get('perfil', [Admin\PerfilController::class, 'index'])->name('perfil');
+    Route::get('perfil',       [Admin\PerfilController::class, 'index'])->name('perfil');
     Route::put('salvarPerfil', [Admin\PerfilController::class, 'save'])->name('perfil.save');
 
-    Route::get('config', [Admin\ConfiguracaoController::class, 'index'])->name('config');
+    Route::get('config',       [Admin\ConfiguracaoController::class, 'index'])->name('config');
     Route::put('salvarConfig', [Admin\ConfiguracaoController::class, 'save'])->name('config.save');
 });
