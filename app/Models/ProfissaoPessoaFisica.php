27c12ba4a69067fb4fe90610af6e44d464e739b0
Vinicius Vitorino <vitorino.developer@outlook.com>
2021-08-11 17:17:43 -0300

    Model de ligação das tabelas pessoa física e profissão.

--- app/Models/ProfissaoPessoaFisica.php
+++ app/Models/ProfissaoPessoaFisica.php
@@ -11,7 +11,22 @@ class ProfissaoPessoaFisica extends Model
 
     protected $table = 'profissao_pessoa_fisica';
 
-    protected $fillable = ['pessoa_fisica_id'];
+    protected $fillable = [
+        'pessoa_fisica_id',
+        'profissao_id',
+        'updated_at',
+        'created_at'
+    ];
 
-    public $timestamp = false;
+    public $timestamps = false;
+
+    public function pessoaFisica()
+    {
+        return $this->belongsTo(PessoaFisica::class);
+    }
+
+    public function profissao()
+    {
+        return $this->belongsTo(Profissao::class);
+    }
 }
