d3a0aea06079c5ccebed9fa403e97e300cde9b8d
Vinicius Vitorino <vitorino.developer@outlook.com>
2021-08-17 17:04:05 -0300

    View do pdf.

---
+++ resources/views/pdf.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PDF</title>
+</head>
+<body>
+    <table>
+        <td>
+            <div>
+                <img src="<?php echo $modelo ?>" margin-left="0px" width="40%" height="45px">
+            </div>
+        </td>
+        <td  align="right">
+            <ul  style="list-style: none">
+                <li><strong>Endereço:</strong>Av. Brig. Lima e Silva, 2035, T.I, Gr. 703/704<br>25 de Agosto, Duque de Caxias/RJ - CEP 25071-181</li>
+                <li><strong>Contatos:</strong>(55-21)2771-5155 - (55-21)96886-5163</li>
+                <li>pecegoebenfica.adv / pecegoebenfica.adv</li>
+                <li>contato@pecegoebenfica.adv.br</li>
+                <li>www.pecegoebenfica.adv.br</li>
+            </ul>
+        </td>
+    </table>
+
+    <table>
+        <td ><strong>Contrato:</strong> Pecego e Benfica Advogados Associados</td>
+        <td>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</td>
+        <td align="right">
+            <ul style="list-style: none">
+                <li><strong>CPF/CNPJ:</strong>  09.321.237/0001-49</li>
+            </ul>
+        </td>
+    </table>
+    <table>
+        <td ><strong>Logradouro:</strong> Av. Brig Lima e Silva</td>
+        <td>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+            &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+        </td>
+        <td align="right">
+            <ul style="list-style: none">
+                <li><strong>Número:</strong>  2035</li>
+            </ul>
+        </td>
+    </table>
+<body>
+</html>
