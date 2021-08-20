ea4aca1019456c0073b505cfc9cac76ff0c8e01f
Vinicius Vitorino <vitorino.developer@outlook.com>
2021-08-20 18:06:54 -0300

    Alteração na variaveis e exclusão de lixos nos códigos.

--- app/Http/Controllers/Admin/CadastroController.php
+++ app/Http/Controllers/Admin/CadastroController.php
@@ -66,12 +66,11 @@ class CadastroController extends Controller
         //dd($request->all());
 
         $cliente                = new Cliente;
-        $cliente                = new PessoaFisica;
-        $cliente                = new PessoaJuridica;
-        $cliente                = new Contato;
-        $cliente                = new Endereco;
-        $processo               = new Processo;
-        $cliente_processo       = new ClienteProcesso;
+        $pf                     = new PessoaFisica;
+        $pj                     = new PessoaJuridica;
+        $contato                = new Contato;
+        $endereco               = new Endereco;
+
         //$profissao_pf   = new ProfissaoPessoaFisica; Pivô entre profissão e pessoa fisica
 
         $cliente->nome              = $request->input('nome');
@@ -80,64 +79,53 @@ class CadastroController extends Controller
 
         if ($cliente->nome = $request->input('nome')){
 
-        $cliente->cpf            = $request->input('cpf');
-        $cliente->pis            = $request->input('pis');
-        $cliente->sexo           = $request->input('sexo');
-        $cliente->profissao      = $request->input('profissao');
-        $cliente->estadoCivil    = $request->input('estadoCivil');
-        $cliente->tratamento     = $request->input('tratamento');
-        $cliente->numCtps        = $request->input('numCtps');
-        $cliente->serieCtps      = $request->input('serieCtps');
-        $cliente->ufCtps         = $request->input('ufCtps');
-        $cliente->nacionalidade  = $request->input('nacionalidade');
-        $cliente->dtNascimento   = $request->input('dtNascimento');
-        $cliente->tituloEleitor  = $request->input('tituloEleitor');
-        $cliente->idtCivil       = $request->input('idtCivil');
-        $cliente->dtExpedicao    = $request->input('dtExpedicao');
-        $cliente->orgExpeditor   = $request->input('orgExpeditor');
-        $cliente->nomeMae        = $request->input('nomeMae');
-        $cliente->cliente()->associate($cliente);
-        $cliente->save();
+        $pf->cpf            = $request->input('cpf');
+        $pf->pis            = $request->input('pis');
+        $pf->sexo           = $request->input('sexo');
+        $pf->profissao      = $request->input('profissao');
+        $pf->estadoCivil    = $request->input('estadoCivil');
+        $pf->tratamento     = $request->input('tratamento');
+        $pf->numCtps        = $request->input('numCtps');
+        $pf->serieCtps      = $request->input('serieCtps');
+        $pf->ufCtps         = $request->input('ufCtps');
+        $pf->nacionalidade  = $request->input('nacionalidade');
+        $pf->dtNascimento   = $request->input('dtNascimento');
+        $pf->tituloEleitor  = $request->input('tituloEleitor');
+        $pf->idtCivil       = $request->input('idtCivil');
+        $pf->dtExpedicao    = $request->input('dtExpedicao');
+        $pf->orgExpeditor   = $request->input('orgExpeditor');
+        $pf->nomeMae        = $request->input('nomeMae');
+        $pf->cliente()->associate($cliente);
+        $pf->save();
 
         }elseif($cliente->nome_empresa = $request->input('nome_empresa')){
 
-        $cliente->numero             = $request->input('numero');
-        $cliente->inscMunicipal      = $request->input('inscMunicipal');
-        $cliente->inscEstadual       = $request->input('inscEstadual');
-        $cliente->codigo             = $request->input('codigo');
-        $cliente->natureza_pj        = $request->input('natureza_pj');
-        $cliente->cliente()->associate($cliente);
-        $cliente->save();
+        $pj->numero             = $request->input('numero');
+        $pj->inscMunicipal      = $request->input('inscMunicipal');
+        $pj->inscEstadual       = $request->input('inscEstadual');
+        $pj->codigo             = $request->input('codigo');
+        $pj->natureza_pj        = $request->input('natureza_pj');
+        $pj->cliente()->associate($cliente);
+        $pj->save();
     }
 
 
-        $cliente->email         = $request->input('email');
-        $cliente->telefone      = $request->input('telefone');
-        $cliente->celular       = $request->input('celular');
-        $cliente->cliente()->associate($cliente);
-        $cliente->save();
+        $contato->email         = $request->input('email');
+        $contato->telefone      = $request->input('telefone');
+        $contato->celular       = $request->input('celular');
+        $contato->cliente()->associate($cliente);
+        $contato->save();
 
 
-        $cliente->logradouro   = $request->input('logradouro');
-        $cliente->complemento  = $request->input('complemento');
-        $cliente->numEndereco  = $request->input('numEndereco');
-        $cliente->bairro       = $request->input('bairro');
-        $cliente->cidade       = $request->input('cidade');
-        $cliente->uf           = $request->input('uf');
-        $cliente->cep          = $request->input('cep');
-        $cliente->cliente()->associate($cliente);
-        $cliente->save();
-
-
-        /*$processo->pasta        = $request->input('pasta');
-        $processo->ultAndamento = $request->input('ultAndamento');
-        $processo->advContrario = $request->input('advContrario');
-        $processo->titulo       = $request->input('titulo');
-        $processo->save();
-
-        $cliente_processo->processo_id->processo()->associate($processo);
-        $cliente_processo->cliente_id->cliente()->associate($cliente);
-        $cliente_processo->save();*/
+        $endereco->logradouro   = $request->input('logradouro');
+        $endereco->complemento  = $request->input('complemento');
+        $endereco->numEndereco  = $request->input('numEndereco');
+        $endereco->bairro       = $request->input('bairro');
+        $endereco->cidade       = $request->input('cidade');
+        $endereco->uf           = $request->input('uf');
+        $endereco->cep          = $request->input('cep');
+        $endereco->cliente()->associate($cliente);
+        $endereco->save();
 
         return redirect()->route('cadastro.index');
     }
@@ -267,115 +255,6 @@ class CadastroController extends Controller
 
             //dd($pessoa_fisica);
         return redirect()->route('cadastro.index');
-
-
-        /*if(!empty($cliente->nome) > 0) {
-            $cliente->nome                          = $request['nome'];
-            $cliente->pessoaFisica->cpf             = $request['cpf'];
-
-            dd($cliente);
-
-        }else{
-            $cliente->nome_empresa                  = $request['nome_empresa'];
-            $cliente->pessoaJuridica->numero        = $request['numero'];
-        }
-
-        $cliente->save();
-
-        //dd($cliente);
-
-        return redirect()->route('cadastro.index');
-
-
-
-        /*$cliente->pessoaFisica->update([
-            'cpf',
-            'pis',
-            'profissao',
-            'estadoCivil',
-            'tratamento',
-            'numCtps',
-            'serieCtps',
-            'ufCtps',
-            'nacionalidade',
-            'dtNascimento',
-            'tituloEleitor',
-            'idtCivil',
-            'dtExpedicao',
-            'nomeMae'
-        ]);
-
-        $cliente->pessoaJuridica->numero             = $request->input('numero');
-        $cliente->pessoaJuridica->inscMunicipal      = $request->input('inscMunicipal');
-        $cliente->pessoaJuridica->inscEstadual       = $request->input('inscEstadual');
-        $cliente->pessoaJuridica->codigo             = $request->input('codigo');
-        $cliente->pessoaJuridica->natureza_pj        = $request->input('natureza_pj');
-
-
-        $cliente->contato->email         = $request->input('email');
-        $cliente->contato->telefone      = $request->input('telefone');
-        $cliente->contato->celular       = $request->input('celular');
-
-
-        $cliente->endereco->logradouro   = $request->input('logradouro');
-        $cliente->endereco->complemento  = $request->input('complemento');
-        $cliente->endereco->numEndereco  = $request->input('numEndereco');
-        $cliente->endereco->bairro       = $request->input('bairro');
-        $cliente->endereco->cidade       = $request->input('cidade');
-        $cliente->endereco->uf           = $request->input('uf');
-        $cliente->endereco->cep          = $request->input('cep');
-
-        $cliente->save();
-
-        return redirect()->route('cadastro.index');
-
-
-        Cliente::findOrFail($id)->update($request->all());
-        if(isset($id->pessoaFisica) > 0){
-            PessoaFisica::findOrFail($id)->update($request->all());
-        }else{
-            PessoaJuridica::findOrFail($id)->update($request->all());
-        }
-        Contato::findOrFail($id)->update($request->all());
-        Endereco::findOrFail($id)->update($request->all());*/
-
-        /*$cliente = Cliente::find($id);
-        if($cliente) {
-            $data = $request->only([
-                'nome'
-            ]);
-
-            $validador = Validator::make([
-                'nome' => $data['nome']
-            ], [
-                'nome' => ['required', 'string', 'max:100']
-            ]);
-
-            $cliente->nome = $data['nome'];
-
-            if($cliente->Contato->email != $data['email']) {
-                $hasEmail = Contato::where('email', $data['email'])->get();
-
-                if(count($hasEmail) === 0) {
-                    $cliente->Contato->email = $data['email'];
-                } else {
-                    $validador->errors()->add('email', __('validation.unique', [
-                        'attribute' => 'email'
-                    ]));
-                }
-            }
-
-            if(count($validador->errors()) > 0) {
-                return redirect()->route('cadastro.edit', [
-                    'cliente' => $id
-                ])->withErrors($validador);
-            }
-
-            $cliente->save();
-
-        }
-
-        return redirect()->route('usuarios.index');*/
     }
 
     public function destroy($id)
