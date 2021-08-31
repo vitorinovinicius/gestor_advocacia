597fdcb2a28d08423f6c509a40b6654d23c8f50b
Vinicius Vitorino <vitorino.developer@outlook.com>
2021-08-31 17:28:59 -0300

    Implementação da página inicial de serviços.

--- resources/views/admin/servicos/index.blade.php
+++ resources/views/admin/servicos/index.blade.php
@@ -1,23 +1,23 @@
 
 @extends('adminlte::page')
 
-@section('title', 'Processos')
+@section('title', 'Serviço')
 
 @section('content_header')
     <h1>
-        Meus processos
-        <a href="{{route('processo.create')}}" class="btn btn-sm btn-success">
-            Novo processo
+        Meus serviços
+        <a href="{{route('servico.create')}}" class="btn btn-sm btn-success">
+            Novo servico
         </a>
     </h1>
 @endsection
 @section('content')
-@if(count($processos) <= 0)
+@if(count(array($servicos)) <= 0)
 <div align="center">
     <div class="col-lg-4 col-4">
         <div class="small-box bg-warning">
             <div class="inner">
-                <h1>Não há processos cadastrados.</h1>
+                <h1>Não há servico cadastrados.</h1>
             </div>
             <div class="icon">
                 <i class="fas fa-exclamation-triangle">
@@ -33,20 +33,20 @@
             <thead>
                 <tr>
                     <th width="100px">Ordem</th>
-                    <th width="600px">@if(count($processos) <= 1)Processo @else Processos @endif</th>
+                    <th width="600px">@if(count($servicos) <= 1)Serviço @else Serviços @endif</th>
                     <th width="200px">Clientes</th>
                     <th width="150px">Ações</th>
                 </tr>
             </thead>
             <tbody>
-                @foreach($processos as $processo)
+                @foreach($servicos as $servico)
                 <tr>
-                    <td>{{$processo->id}}</td>
-                    <td><a href="{{route('processo.show', [$processo->id])}}">{{$processo->pasta}}</a></td>
-                    <td>@if(array($processo) > 0){{count($processo->cliente)}} @else Não há clientes registrados. @endif</td>
+                    <td>{{$servico->id}}</td>
+                    <td><a href="{{route('servico.show', [$servico->id])}}">{{$servico->pasta_servico}}</a></td>
+                    <td>@if(array($servico) > 0){{count($servico->cliente)}} @else Não há clientes registrados. @endif</td>
                     <td>
                         <a href="#" class="btn btn-sm btn-warning">Editar</a>
-                        <form class="d-inline" method="POST" action="{{route('processo.destroy', $processo->id)}}" onsubmit="return confirm('Isso irá excluir, deseja continuar?')" >
+                        <form class="d-inline" method="POST" action="{{route('servico.destroy', $servico->id)}}" onsubmit="return confirm('Isso irá excluir, deseja continuar?')" >
                             @method('delete')
                             @csrf
                             <button class="btn btn-sm btn-danger">Excluir</button>
@@ -56,7 +56,7 @@
                 @endforeach
             </tbody>
         </table>
-        {{$processos->links()}}
+        {{$servicos->links()}}
     </div>
 </div>
 @endif
