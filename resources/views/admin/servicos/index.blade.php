80e28dcf83e376566799e3d9b05b5ff8dc97f266
Vinicius Vitorino <vitorino.developer@outlook.com>
2021-09-21 18:07:33 -0300

    Alteração na geração de dados da tabela.

--- resources/views/admin/servicos/index.blade.php
+++ resources/views/admin/servicos/index.blade.php
@@ -35,17 +35,15 @@
         <table class="table table-hover" id="servicos">
             <thead>
                 <tr>
-                    <th width="100px">Ordem</th>
-                    <th width="600px">@if(count($servicos) <= 1)Serviço @else Serviços @endif</th>
-                    <th width="200px">Clientes</th>
-                    <th width="150px">Ações</th>
+                    <th class="col-sm-7">@if(count($servicos) <= 1)Pasta do serviço @else Pastas dos serviços @endif</th>
+                    <th class="col-sm-3">Clientes</th>
+                    <th class="col-sm-2">Ações</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach($servicos as $servico)
                 <tr>
-                    <td>{{$servico->id}}</td>
-                    <td><a href="{{route('servico.show', [$servico->id])}}">{{$servico->pasta_servico}}</a></td>
+                    <td><a href="{{route('servico.show', [$servico->id])}}">{{$servico->nome_servico}}</a></td>
                     <td>@if(isset($servico->cliente) > 0){{count($servico->cliente) > 0}} @if(count($servico->cliente) === 0)Não há cliente registrado. @endif @endif</td>
                     <td>
                         <a href="#" class="btn btn-sm btn-warning">Editar</a>
